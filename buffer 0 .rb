92da66f1a9a3fe5a47e6e82ec7c552a746aef8ad
10011010 <83015481+10011010@users.noreply.github.com>
2021-04-23 12:03:54 +0100

    Add files via upload

---
+++ buffer 0 .rb
@@ -0,0 +1,190 @@
+use_bpm 118
+define :rando do
+  n = 
+  use_random_seed n
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rando
+in_thread do
+  
+  if one_in(6)
+    d1
+  else
+    sleep 0
+  end
+  
+  if one_in(6)
+    d2
+  else
+    sleep 0
+  end
+  
+  if one_in(6)
+    d3
+    
+  else
+    sleep 0
+  end
+  
+  if one_in(6)
+    d4
+  else
+    sleep (0)
+  end
+  
+  if one_in(6)
+    d5
+  else
+    sleep 0
+  end
+  
+  if one_in(6)
+    d6
+  else
+    sleep 0
+  end
+  
+  if one_in (2)
+    d1
+  else
+    d3
+  end
+end
+
+
+
+
+
+#BASS
+in_thread do
+  
+  if one_in (3)
+    b1
+  else
+    sleep 0
+  end
+  
+  if one_in (3)
+    b2
+  else
+    sleep 0
+  end
+  
+  if one_in (3)
+    b3
+  else
+    sleep 0
+  end
+  
+  if one_in (2)
+    b1
+  else
+    b3
+  end
+end
+
+
+#MELODY
+
+in_thread do
+  if one_in (3)
+    m2
+  else
+    sleep 0
+  end
+  
+  
+  if one_in (3)
+    m3
+  else
+    sleep 0
+  end
+  
+  if one_in(3)
+    m4
+  else
+    sleep 0
+  end
+  
+  if one_in(4)
+    p1
+  else
+    p2
+  end
+end
+
+
+
